e178959f7f41104a8cbd554469602771484bbce4
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-09-27 10:15:41 -0500

    rubocop changes

--- lesson_2/rpsls.rb
+++ lesson_2/rpsls.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal:true
+
 system 'clear'
 
 def prompt(message)
@@ -9,23 +10,15 @@ def abb
   puts 'R=Rock, P=Paper, S=Scissors, L=Lizard, Sp=Spock'
 end
 
-def abb_converter(abb)
-  case abb
-  when 'r'
-    'Rock'
-  when 'p'
-    'Paper'
-  when 's'
-    'Scissors'
-  when 'l'
-    'Lizard'
-  when 'sp'
-    'Spock'
-  end
+def abb_converter(ans)
+  abb = { r: 'rock', p: 'paper', s: 'scissors', l: 'lizard', sp: 'spock' }
+  abb[ans.to_sym]
 end
 
 def print_player_winner_phrases
-  puts ["My God, man! You're good", 'You have boldy gone where no man has gone before', "You're doing the impossible!"].sample
+  puts ["My God, man! You're good",
+        'You have boldy gone where no man has gone before',
+        "You're doing the impossible!"].sample
 end
 
 def print_computer_winner_phrases
@@ -37,52 +30,36 @@ def valid_choice?(choice)
 end
 
 def win?(first, second)
-  (first == 's' && %w[p l].include?(second)) ||
-  (first == 'p' && %w[r sp].include?(second)) ||
-  (first == 'r' && %w[s l].include?(second)) ||
-  (first == 'l' && %w[sp p].include?(second)) ||
-  (first == 'sp' && %w[s r].include?(second))
+  win = { s: %w[p l], p: %w[r sp], r: %w[s l], l: %w[s p], sp: %w[s r] }
+  win[first.to_sym].include?(second)
 end
 
-def description(a,b)
-  if (a == 's' || b == 's') && (a == 'p' || b == 'p')
-    puts 'Scissors cuts Paper!'
-  elsif (a == 's' || b == 's') && (a == 'l' || b == 'l')
-    puts 'Scissors decapitates Lizard!'
-  elsif (a == 'p' || b == 'p') && (a == 'r' || b == 'r')
-    puts 'Paper covers Rock!'
-  elsif (a == 'p' || b == 'p') && (a == 'sp' || b == 'sp')
-    puts 'Paper disproves Spock!'
-  elsif (a == 'r' || b == 'r') && (a == 's' || b == 's')
-    puts 'Rock crushes scissors!'
-  elsif (a == 'r' || b == 'r') && (a == 'l' || b == 'l')
-    puts 'Rock crushes Lizard!'
-  elsif (a == 'l' || b == 'l') && (a == 'sp' || b == 'sp')
-    puts "Lizard poisens Spock!"
-  elsif (a == 'l' || b == 'l') && (a == 'p' || b == 'p')
-    puts 'Lizard eats Paper!'
-  elsif (a == 'sp' || b == 'sp') && (a == 's' || b == 's')
-    puts 'Spock smashes Scissors!'
-  elsif (a == 'sp' || b == 'sp') && (a == 'r' || b == 'r')
-    puts 'Spock vaporizes Rock!'
+def print_description(user, comp)
+  phrases = { %w[s p] => 'Scissors cuts Paper!', %w[s l] => 'Scissors decapitates Lizard!',
+              %w[p r] => 'Paper covers Rock!', %w[p sp] => 'Paper disproves Spock!',
+              %w[r s] => 'Rock crushes Scissors!', %w[r l] => 'Rock crushes Lizard!',
+              %w[l sp] => 'Lizard poisens Spock!', %w[l p] => 'Lizard eats Paper!',
+              %w[sp s] => 'Spock smashes Scissors!', %w[sp r] => 'Spock vaporizes Rock!' }
+  phrases.select do |choices, description|
+    puts description if choices.include?(user) && choices.include?(comp)
   end
 end
 
 rules = <<~MSG
-Scissors cuts Paper covers Rock crushes
-Lizard poisens Spock smashes Scissors
-decapitates Lizard eats Paper disproves
-Spock vaporizes Rock crushes Scissors
+  Scissors cuts Paper covers Rock crushes
+  Lizard poisens Spock smashes Scissors
+  decapitates Lizard eats Paper disproves
+  Spock vaporizes Rock crushes Scissors
 
-Type any key to continue
+  Type any key to continue
 MSG
 
-welcome_message = <<-MSG
-****Get those Spock brows ready...
-**It's time to play...
-**ROCK,PAPER,SCISSORS,LIZARD,SPOCK!
+welcome_message = <<~MSG
+  ****Get those Spock brows ready...
+  **It's time to play...
+  **ROCK,PAPER,SCISSORS,LIZARD,SPOCK!
 
-Lets jump in.. type any key to continue
+  Lets jump in.. type any key to continue
 MSG
 
 puts welcome_message
@@ -94,19 +71,18 @@ gets.chomp
 system 'clear'
 
 got_it = <<~MSG
-Got it? 
-Good! 
-Let's begin!
-*
-*
-*
-*
-type any key to continue
+  Got it?
+  Good!
+  Let's begin!
+  *
+  *
+  *
+  *
+  type any key to continue
 MSG
 puts got_it
 gets.chomp
 system 'clear'
-
 
 loop do
   star_treck_characters = %w[Captain\ Kirk Spock Nyota Scotty Hikaru].sample
@@ -120,19 +96,18 @@ loop do
       prompt "Execute! #{abb}"
       user_choice = gets.chomp
       system 'clear'
-      if valid_choice?(user_choice)
-        break
-      else
-        prompt "Try again..#{abb}"
-      end
+      break if valid_choice?(user_choice)
+
+      prompt 'Try again..'
     end
     user_choice = user_choice.downcase
 
     computer_choice = %w[r p s l sp].sample
 
-    prompt "You chose #{abb_converter(user_choice)}, and #{star_treck_characters} chose #{abb_converter(computer_choice)}."
-    
-    description(user_choice, computer_choice)
+    prompt "You chose #{abb_converter(user_choice.to_s)}, and #{star_treck_characters}\
+ chose #{abb_converter(computer_choice.to_s)}."
+
+    print_description(user_choice, computer_choice) unless user_choice == computer_choice
 
     if win?(user_choice, computer_choice)
       user_win_counter += 1
@@ -149,17 +124,16 @@ loop do
     You: #{user_win_counter}
     #{star_treck_characters}: #{computer_win_counter}
     MSG
-    
+
     puts display_scorecard
     prompt 'You won! Highly illogical' if user_win_counter == 3
     prompt "You've been annihilated! Team #{star_treck_characters}!!" if computer_win_counter == 3
     break if user_win_counter == 3 || computer_win_counter == 3
   end
   prompt 'Challenge another character? y/n'
-  ans = ''
   ans = gets.chomp
   break unless ans.downcase.start_with? 'y'
-  
+
   system 'clear'
 end
 
