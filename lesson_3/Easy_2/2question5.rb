588850877b62a6bc470da0776b2fb2d2acc2e742
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-09-28 09:51:39 -0500

    dir change

---
+++ lesson_3/Easy_2/2question5.rb
@@ -0,0 +1,5 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones1=%w[Fred Barney Wilma Betty BamBam Pebbles]
+
+puts flintstones1
---
+++ lesson_3/Easy_2/2question7.rb
@@ -0,0 +1,5 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones.push("dino", "hoppy")
+
+p flintstones.concat(%w(dino hoppy))
