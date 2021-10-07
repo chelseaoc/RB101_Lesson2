47e5afedf8857c496781b0a5bbf55bb12de05531
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-10-07 09:17:09 -0500

    output array

--- lesson_4/palindrome.rb
+++ lesson_4/palindrome.rb
@@ -1,16 +1,18 @@
 def palindrome(word)
+  pal=[]
   loop do
     counter=1
     break if word.nil?
     while counter<word.length
       if word[0..counter]==word[0..counter].reverse
-        puts word[0..counter]
+        pal<<word[0..counter]
       end
         counter+=1
     end
     break if word.length==1
     word=word[1..-1]
   end
+  p pal
 end
 
 palindrome("abcdcbaaskdwjhusfabcba")
