1da81773d6c058bf97d4ff211b150a5799d21eb6
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-10-08 10:26:06 -0500

    format

--- lesson_4/letters_in_a_string.rb
+++ lesson_4/letters_in_a_string.rb
@@ -16,9 +16,11 @@ def letters_in_a_string(string,letter)
   puts "There are #{my_letters[letter]} #{letter}'s in the string."
 end
 
-x="Traditionally, a text is understood to be a piece of written or spoken material in its primary form (as opposed to a paraphrase or summary). A text is any stretch of language that can be understood in context. It may be as simple as 1-2 words (such as a stop sign) or as complex as a novel. Any sequence of sentences that belong together can be considered a text. you would be referring to the words in the book, not the physical book itself. Information related to a text, and often printed alongside it—such as an author's name, the publisher, the date of publication, etc.—is known as paratext. The idea of what constitutes a text has evolved over time. In recent years, the dynamics of technology—especially social media—have expanded the notion of the text to include symbols such as emoticons and emojis. A sociologist studying teenage communication, for example, might refer to texts that combine traditional language and graphic symbols."
+x="Traditionally, a text is understood to be a piece of written or spoken material in its \n primary form (as opposed to a paraphrase or summary). A text is any stretch of \n language that can be understood in context. It may be as simple as 1-2 words (such as \n a stop sign) or as complex as a novel. Any sequence of sentences that belong together \n can be considered a text. you would be referring to the words in the book, not the \n physical book itself. Information related to a text, and often printed alongside it—such \n as an author's name, the publisher, the date of publication, etc.—is known as \n paratext. The idea of what constitutes a text has evolved over time. In recent years, \n the dynamics of technology—especially social media—have expanded the notion of the text \n to include symbols such as emoticons and emojis. A sociologist studying teenage \n communication, for example, might refer to texts that combine traditional language and \n graphic symbols."
 
 letters_in_a_string(x,"h")
 letters_in_a_string(x," ")
 letters_in_a_string(x,"r")
 letters_in_a_string(x,2)
+
+
