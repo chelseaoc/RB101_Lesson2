cb84150527d857f5fbd728aff50d2de450796ee9
Chelsea O'Connor <Chelseaaoconnor1@gmail.com>
2021-11-03 14:02:20 -0500

    BONUS

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -1,4 +1,5 @@
-require "pry"
+GAME_TO_NUMBER = 21
+COMPUTER_HIT_TO = 16
 
 def prompt(message)
   puts "---->>#{message}"
@@ -38,11 +39,12 @@ def assign_values_to_cards(full_deck)
   hash_values
 end
 
-# prints out card choice
-def print_your_card_is(user, card)
-  num = card[0]
-  suit = card[1]
-  prompt "#{user} card is #{a_an(num)} #{num} of #{suit}"
+def show_all_cards(word, player_cards, points)
+  puts "#{word} cards are.."
+  puts '*********************'
+  player_cards.keys.map { |num, suit| puts "#{num} of #{suit}" }
+  puts "********************* VALUE of #{points}"
+  yadayada
 end
 
 def a_an(num)
@@ -69,18 +71,13 @@ def determine_value(hash, card)
 end
 
 def yadayada
-  print <<~MSG
-  -  -  -  -  -  -  -  -
-  -  -  -  -  -  -  -  -
-  -  -  -  -  -  -  -  -
-  -  -  -  -  -  -  -  -
-  MSG
+  2.times { puts "\n" }
 end
 
 def score(cards)
   total = cards.values.inject(:+)
   cards.map do |card, value|
-    if card[0] == 'Ace' && value == 11 && total > 21
+    if card[0] == 'Ace' && value == 11 && total > GAME_TO_NUMBER
       cards[card] = 1
       total = cards.values.inject(:+)
     end
@@ -98,85 +95,110 @@ end
 
 # true/false bust?
 def bust?(score)
-  score > 21
+  score > GAME_TO_NUMBER
 end
 
 clear
-prompt "Welcome to 21! Type any key to continue"
+prompt "Welcome to #{GAME_TO_NUMBER}! Tournament is best out of 5."
+puts 'Type any key to continue'
 gets.chomp
 clear
+
 loop do
-  full_deck = deck_initialize
-  hash = assign_values_to_cards(full_deck)
-  user_cards = {}
-  comp_cards = {}
-  # pick initial 2 cards for user and computer
-  user_points = ''
-  comp_points = ''
+  comp_winner_count = 0
+  user_winner_count = 0
   loop do
-    user_points = player_points(user_cards, full_deck, hash)
-    comp_points = player_points(comp_cards, full_deck, hash)
-    break if comp_cards.length == 2
-  end
-  # show results of first cards
-  print_your_card_is('Your first', user_cards.keys[0])
-  print_your_card_is('Your second', user_cards.keys[1])
-  puts "You have a total value of #{user_points}"
-  yadayada
-  print_your_card_is("Dealer's first", comp_cards.keys[0])
-  yadayada
-  user_card_key_number = 1
-  loop do
-    answer = ''
+    full_deck = deck_initialize
+    hash = assign_values_to_cards(full_deck)
+    user_cards = {}
+    comp_cards = {}
+    # pick initial 2 cards for user and computer
+    user_points = ''
+    comp_points = ''
     loop do
-      prompt "Would you like to Hit or Stay?"
-      answer = gets.chomp.downcase
-      break if %w(s stay h hit).include?(answer)
-      prompt 'Invalid Response'
+      user_points = player_points(user_cards, full_deck, hash)
+      comp_points = player_points(comp_cards, full_deck, hash)
+      break if comp_cards.length == 2
     end
-    break if %w(s stay).include?(answer)
-    user_points = player_points(user_cards, full_deck, hash)
+    # show results of first cards
+    show_all_cards('Your', user_cards, user_points)
+    puts "Dealer's first card is #{a_an(comp_cards.keys[0][0])}"
+    puts '*********************'
+    puts "#{comp_cards.keys[0][0]} of #{comp_cards.keys[0][1]}"
+    puts "********************* VALUE of #{comp_cards.values[0]}"
+    user_card_key_number = 1
+    loop do
+      answer = ''
+      loop do
+        yadayada
+        prompt "Would you like to Hit or Stay?"
+        answer = gets.chomp.downcase
+        break if %w(s stay h hit).include?(answer)
+        prompt 'Invalid Response'
+      end
+      break if %w(s stay).include?(answer)
+      user_points = player_points(user_cards, full_deck, hash)
+      if bust?(user_points)
+        break
+      end
+      clear
+      puts "Your card is #{a_an(user_cards.keys[user_card_key_number += 1])} \
+#{user_cards.keys[user_card_key_number][0]} of \
+#{user_cards.keys[user_card_key_number][1]}"
+      show_all_cards('Your', user_cards, user_points)
+      if comp_points == COMPUTER_HIT_TO
+        comp_points = player_points(comp_cards, full_deck, hash)
+        if bust?(comp_points)
+          break
+        end
+      end
+    end
+    unless bust?(user_points)
+      loop do
+        break if comp_points == COMPUTER_HIT_TO
+        comp_points = player_points(comp_cards, full_deck, hash)
+        if bust?(comp_points)
+          break
+        end
+      end
+    end
+    yadayada
+    clear
+    show_all_cards('Your', user_cards, user_points)
+    show_all_cards("Dealer's", comp_cards, comp_points)
     if bust?(user_points)
+      prompt 'You Bust! Dealer Wins!'
+      comp_winner_count += 1
+    elsif bust?(comp_points)
+      prompt 'Dealer Bust! You Win!'
+      user_winner_count += 1
+    elsif comp_points == user_points
+      prompt 'PUSH'
+    elsif user_points > comp_points
+      prompt 'You Win!'
+      user_winner_count += 1
+    else
+      prompt 'Dealer Wins!'
+      comp_winner_count += 1
+    end
+    puts '*********************'
+    prompt "GAME SCORE: You: #{user_winner_count} Dealer: #{comp_winner_count}"
+    yadayada
+    prompt "Type any key to continue"
+    gets.chomp
+    clear
+    if user_winner_count == 5
+      prompt "You Win the Tournament!"
+      break
+    elsif comp_winner_count == 5
+      prompt "Dealer Wins the Tournament!"
       break
     end
-
-    clear
-    print_your_card_is('Your', user_cards.keys[user_card_key_number += 1])
-    prompt "You have a total value of #{user_points}"
-    if comp_points <= 17
-      comp_points = player_points(comp_cards, full_deck, hash)
-      if bust?(comp_points)
-        break
-      end
-    end
   end
-  unless bust?(user_points)
-    loop do
-      break if comp_points >= 17
-      comp_points = player_points(comp_cards, full_deck, hash)
-      if bust?(comp_points)
-        break
-      end
-    end
-  end
-  yadayada
-  clear
-  prompt "You have a total value of #{user_points}"
-  prompt "Dealer has a total value of #{comp_points}"
-  yadayada
-  if bust?(user_points)
-    prompt 'You Bust! Dealer Wins!'
-  elsif bust?(comp_points)
-    prompt 'Dealer Bust! You Win!'
-  elsif comp_points == user_points
-    prompt 'PUSH'
-  elsif user_points > comp_points
-    prompt 'You Win!'
-  else
-    prompt 'Dealer Wins!'
-  end
-  prompt "Play Again?"
-  answer = gets.chomp.downcase
-  break if %w(n no).include?(answer)
+  prompt "Do you want to play again?"
+  answer = gets.chomp
+  break if %w(n no).include?(answer.downcase)
   clear
 end
+prompt "Thanks for playing!"
+yadayada
